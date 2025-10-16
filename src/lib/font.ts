c1e33eda109830f47d8e09ed09f29ecb0be2d5e2
Pranjal Jawalkar <pranjaljawalkar06@gmail.com>
2025-10-16 19:48:42 +0530

    font changed

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,11 +1,12 @@
 import localFont from 'next/font/local';
 
 export const arena = localFont({
-  src: '../../public/ARENACB.woff2',  // 👈 Full relative path from src/lib
+  
+  src: '../../public/name.woff2',  // 👈 Full relative path from src/lib
   display: 'swap',
 });
 
 export const customName = localFont({
-  src: '../../public/name.woff2',  // 👈 Full relative path from src/lib
+   src: '../../public/ARENACB.woff2', // 👈 Full relative path from src/lib
   display: 'swap',
 });
